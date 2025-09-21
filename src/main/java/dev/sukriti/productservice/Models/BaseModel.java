05db97b19c39de4ec6ca3ddedbccc6b1ad517a7d
Sukriti Srivastava <sukritisrivastava02@gmail.com>
2025-09-21 16:36:24 +0530

    Added Dependencies and Model class

---
+++ src/main/java/dev/sukriti/productservice/Models/BaseModel.java
@@ -0,0 +1,14 @@
+package dev.sukriti.productservice.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Getter
+@Setter
+public class BaseModel {
+    private Long id;
+    private Date createdAt;
+    private Date lastUpdatedAt;
+    private boolean isDeleted;
+}
